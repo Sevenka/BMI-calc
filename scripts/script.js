8091b99ad30496eb220ea1008e66767a6d3f7c13
Oksana Mazun <misssevenka@mail.ru>
2016-01-22 21:56:18 +0200

    Added verification of data

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,9 @@ function calc(button){
 	var weight = document.getElementById('weight').value;
 	height*=0.01;
 	var result=(weight/(height*height)).toFixed(1);
-	document.getElementById('bmi').innerHTML='<p>Your BMI is '+result+'</p>';
-	/*document.body.innerHTML+='<p>Your BMI is '+result+'</p>';*/
+	if (height<1||height>2.4||weight<15||isFinite(result)==false){
+		document.getElementById('bmi').innerHTML='<p>Please, input the correct data</p>';
+	} else {
+		document.getElementById('bmi').innerHTML='<p>Your BMI is '+result+'</p>';
+	}
 };
