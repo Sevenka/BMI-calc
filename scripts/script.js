ee95b1ebd76bb6840b1affd941110e8241947bb5
Oksana Mazun <misssevenka@mail.ru>
2016-01-22 22:13:03 +0200

    Added limit of weight (max weight - 500). Deleted useless action.

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,10 @@
 function calc(button){
-	var height = document.getElementById('height').value;
+	var height = (document.getElementById('height').value)*0.01;
 	var weight = document.getElementById('weight').value;
-	height*=0.01;
 	var result=(weight/(height*height)).toFixed(1);
-	if (height<1||height>2.4||weight<15||isFinite(result)==false){
+	if (height<1||height>2.4||weight<15||weight>500||isFinite(result)==false){
 		document.getElementById('bmi').innerHTML='<p>Please, input the correct data</p>';
 	} else {
 		document.getElementById('bmi').innerHTML='<p>Your BMI is '+result+'</p>';
-	}
+	};
 };
